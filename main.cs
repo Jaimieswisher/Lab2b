416da003e91f42f335a62a2c7c36e2e8ffca890e
Jaimie Swisher <Jaimieswisher@yahoo.com>
2021-02-02 23:31:15 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,16 @@
+{
+  {
+    x = 6;
+    y = 3;
+    var x = Console.ReadLine();
+    var y = Console.ReadLine();
+    int x1 = Convert.ToInt32(x);
+    int y1 = Convert.ToInt32(y);
+    
+    Console.WriteLine ("Sum = (x+y)");
+    Console.WriteLine ("Diff = (x-y)");
+    Console.WriteLine ("Product = (x*y)");
+    Console.WriteLine ("Quotient = (x/y)");
+    Console.WriteLine ("Remainder = (x%y)");
+    }
+}
